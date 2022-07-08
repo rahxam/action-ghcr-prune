5de66ee127120b02ace24b78095720f75fc59e14
Maximilian Hartig <maximilian.hartig@quadrio.de>
2022-07-08 15:38:28 +0200

    Add logs

--- src/version-filter.js
+++ src/version-filter.js
@@ -1,46 +1,47 @@
-const MS_IN_DAY = 1000 * 60 * 60 * 24;
+const MS_IN_DAY = 1000 * 60 * 60 * 24
 
-const daysBetween = (startDate, endDate = new Date()) => Math.floor((endDate.getTime() - startDate.getTime()) / MS_IN_DAY);
+const daysBetween = (startDate, endDate = new Date()) =>
+  Math.floor((endDate.getTime() - startDate.getTime()) / MS_IN_DAY)
 
-const anyRegexMatch = (regexes) => (tags) =>
-  regexes.some((regex) => tags.some((tag) => tag.match(regex)));
+const anyRegexMatch = regexes => tags =>
+  regexes.some(regex => tags.some(tag => tag.match(regex)))
 
-const versionFilter = (options) => (version) => {
-  const {
-    olderThan,
-    untagged,
-    tagRegex,
-    keepTags,
-    keepTagsRegexes,
-  } = options;
-  const createdAt = new Date(version.created_at);
-  const age = daysBetween(createdAt);
+const versionFilter = options => version => {
+  const { olderThan, untagged, tagRegex, keepTags, keepTagsRegexes } = options
+  const createdAt = new Date(version.created_at)
+  const age = daysBetween(createdAt)
 
   if (olderThan > age) {
-    return false;
+    console.log('older', olderThan, age)
+    return false
   }
 
-  const tags = version.metadata.container.tags;
+  const tags = version.metadata.container.tags
+  console.log(tags)
 
   if (untagged && (!tags || !tags.length)) {
-    return true;
+    console.log('untagged')
+    return true
   }
 
-  if (keepTags && tags && keepTags.some((keepTag) => tags.includes(keepTag))) {
-    return false;
+  if (keepTags && tags && keepTags.some(keepTag => tags.includes(keepTag))) {
+    console.log('keepTags')
+    return false
   }
 
   if (keepTagsRegexes && tags && anyRegexMatch(keepTagsRegexes)(tags)) {
-    return false;
+    console.log('keepTagsRegexes')
+    return false
   }
 
-  if (tagRegex && tags && tags.some((tag) => tag.match(tagRegex))) {
-    return true;
+  if (tagRegex && tags && tags.some(tag => tag.match(tagRegex))) {
+    console.log('tagRegex')
+    return true
   }
 
-  return false;
-};
+  return false
+}
 
 module.exports = {
-  versionFilter,
-};
+  versionFilter
+}
